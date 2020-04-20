33f54b17ffcda004ec86f5dcbdba898f3cb4e879
Danila Leontyev <danila.leontyev.web@gmail.com>
2020-04-21 00:41:34 +0500

    npm init and creaet print function

--- index.ts
+++ index.ts
@@ -1,1 +1,43 @@
-console.log('slkdfj')
+type Cell = {
+  name: string;
+}
+
+type Board = Cell[][]
+
+const board: Board = []
+
+const initialBoard = [
+  [{ name: '0' }, { name: '1' }, { name: '0' },],
+  [{ name: '1' }, { name: '1' }, { name: '1' },],
+  [{ name: '1' }, { name: '1' }, { name: '1' },],
+  [{ name: '1' }, { name: '1' }, { name: '1' },],
+  [{ name: '0' }, { name: '1' }, { name: '0' },],
+]
+
+const nextTick = (prevBoard: Board): Board => {
+  const newBoard = prevBoard;
+  prevBoard.forEach((line, y) => {
+    line.forEach((cell, x) => {
+      checkNeighbors(x, y, newBoard)
+    })
+  })
+  return newBoard;
+}
+
+const checkNeighbors = (row: number, column: number, prevBoard: Board) => {
+
+}
+
+const printBoard = (board: Board): void => {
+  board.forEach((row) => {
+    const line = []
+    row.forEach((cell) => {
+      line.push(cell.name)
+    })
+    line.join()
+    console.log(line);
+  })
+}
+
+const newBoard = nextTick(initialBoard);
+printBoard(newBoard);
